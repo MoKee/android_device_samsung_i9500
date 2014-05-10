cb183e330c27fd8cf59170c8694f0fd40d2b3ba1
martincz <martincz@mokeedev.com>
2014-05-10 11:54:10 +0800

    i9500: mokee bringup
    Change-Id: Iefda472ce07e8646a85822a0b5ed19c6b0001bc1

--- mokee.mk
+++ mokee.mk
@@ -1,15 +1,15 @@
 $(call inherit-product, device/samsung/i9500/full_i9500.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit some common MK stuff
+$(call inherit-product, vendor/mk/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_i9500
+PRODUCT_NAME := mk_i9500
 PRODUCT_DEVICE := i9500
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
